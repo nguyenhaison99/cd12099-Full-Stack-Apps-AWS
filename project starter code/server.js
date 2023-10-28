8b02cd32964d866cfa3f82e6f4695726ae529e62
nguyenhaison99 <nguyenhaison7@gmail.com>
2023-10-28 17:28:14 +0700

    Up

--- project starter code/server.js
+++ project starter code/server.js
@@ -16,20 +16,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get('/filteredimage', async (req, res) => {
 
-    /**************************************************************************** */
+    // 1. validate the image_url query
+    try {
+      const { image_url } = req.query;
+      if (!image_url) {
+        console.error('Image URL is missing or malformed');
+        return res.status(400).send({ message: 'Image URL is required or malformed' });
+      }
 
-  //! END @TODO1
+      // 2. call filterImageFromURL(image_url) to filter the image
+      const localImagePath = await filterImageFromURL(image_url);
+
+      // 3. send the resulting file in the response
+      res.sendFile(localImagePath, (err) => {
+        if (err) {
+          console.error('Error sending the processed image:', err);
+        }
+
+        // 4. deletes any files on the server on finish of the response
+        deleteLocalFiles([localImagePath]);
+      });
+    } catch (error) {
+      // Log any unhandled errors
+      console.error('An unexpected error occurred:', error);
+      res.status(500).send({ message: 'An unexpected error occurred' });
+    }
+  });
+
   
   // Root Endpoint
   // Displays a simple message to the user
